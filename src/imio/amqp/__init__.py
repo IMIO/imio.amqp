4230849f94e9012b8ca375821ab4997b3380881b
Martin Peeters <martin.peeters@affinitic.be>
2014-06-05 17:33:01 +0200

    Add BaseSingleMessagePublisher to api

--- src/imio/amqp/__init__.py
+++ src/imio/amqp/__init__.py
@@ -3,10 +3,12 @@
 from imio.amqp.consumer import BaseConsumer
 from imio.amqp.dispatcher import BaseDispatcher
 from imio.amqp.publisher import BasePublisher
+from imio.amqp.publisher import BaseSingleMessagePublisher
 
 
 __all__ = (
     BaseConsumer.__name__,
     BaseDispatcher.__name__,
     BasePublisher.__name__,
+    BaseSingleMessagePublisher.__name__,
 )
